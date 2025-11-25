391a9d7949c9422ac44779f8f0000cb89374c3ca
Rasheed Juma <rashidj.mwinyi@gmail.com>
2025-11-25 08:36:50 +0100

    Delete apps directory

--- apps/core/urls.py
+++
@@ -1,9 +0,0 @@
-from accounts.views import clear_all_sessions
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    
-    path('emergency-clear-sessions/', clear_all_sessions, name='clear_sessions'),
-    
-]
