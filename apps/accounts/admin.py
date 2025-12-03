9f55cc540f54da966e4339d6629592ba4da93f4c
Rasheed Juma <rashidj.mwinyi@gmail.com>
2025-12-03 14:44:03 +0100

    Update admin.py

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,17 +1,3 @@
-# from django.contrib import admin
-# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-# from .models import User
-
-
-# @admin.register(User)
-# class UserAdmin(BaseUserAdmin):
-#     fieldsets = BaseUserAdmin.fieldsets + (
-#         ('Additional Info', {'fields': ('phone', 'role', 'assigned_warehouse')}),  # Added assigned_warehouse here
-#     )
-#     list_display = ['username', 'email', 'role', 'assigned_warehouse', 'is_staff']  # Added assigned_warehouse here
-#     list_filter = ['role', 'is_staff', 'is_superuser', 'assigned_warehouse']  # Added assigned_warehouse here
-
-# apps/accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.html import format_html
